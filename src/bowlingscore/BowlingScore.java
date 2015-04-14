ad44f648988329a671ed6b790bc2cdc43ece7fe9
asik <asikpradhan@gmail.com>
2015-04-14 15:14:48 -0400

    Test for First ball strike pass

--- src/bowlingscore/BowlingScore.java
+++ src/bowlingscore/BowlingScore.java
@@ -15,17 +15,21 @@ public class BowlingScore {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
     }
-    
+
     public static int calculuateGameScore(ScoreSheet sheet) {
         int value = 0;
-        
-        for (FrameScore fs: sheet.getFrameScores()) {
-            value = fs.getFirstBall() + fs.getSecondBall();
+        for (int i = 0; i < sheet.getFrameScores().size(); i++) {
+            if (10 == sheet.getFrameScores().get(i).getFirstBall()) {
+                value += 10 + (sheet.getFrameScores().get(i + 1).getFirstBall() + sheet.getFrameScores().get(i + 1).getSecondBall());
+            } else {
+                value += sheet.getFrameScores().get(i).getFirstBall() + sheet.getFrameScores().get(i).getSecondBall();
+            }
+
         }
-        
+
         return value;
     }
-    
+
 }
--- test/bowlingscore/BowlingScoreTest.java
+++ test/bowlingscore/BowlingScoreTest.java
@@ -15,7 +15,7 @@ import static org.junit.Assert.*;
  * @author asik
  */
 public class BowlingScoreTest {
-    
+
     public BowlingScoreTest() {
     }
 
@@ -24,50 +24,89 @@ public class BowlingScoreTest {
      */
     @Test
     public void testFrameOneBallOne() {
-        
+
         ScoreSheet scoreSheet = new ScoreSheet();
         FrameScore fs = new FrameScore();
         List<FrameScore> fsList = new ArrayList<>();
-        
+
         fs.setFirstBall(3);
         fsList.add(fs);
         scoreSheet.setFrameScores(fsList);
-        
+
         assertEquals(3, BowlingScore.calculuateGameScore(scoreSheet));
-        
+
     }
-    
+
     @Test
     public void testFrameOneBallTwo() {
         ScoreSheet scoreSheet = new ScoreSheet();
         FrameScore fs = new FrameScore();
         List<FrameScore> fsList = new ArrayList<>();
-        
+
         fs.setFirstBall(3);
         fs.setSecondBall(5);
         fsList.add(fs);
         scoreSheet.setFrameScores(fsList);
-        
+
         assertEquals(8, BowlingScore.calculuateGameScore(scoreSheet));
     }
-    
+
     @Test
     public void testFrameTwoBallOne() {
         ScoreSheet scoreSheet = new ScoreSheet();
         List<FrameScore> fsList = new ArrayList<>();
-        
+
         FrameScore fs1 = new FrameScore();
         fs1.setFirstBall(3);
         fs1.setSecondBall(5);
         fsList.add(fs1);
-        
+
         FrameScore fs2 = new FrameScore();
         fs2.setFirstBall(3);
         fsList.add(fs2);
-        
+
         scoreSheet.setFrameScores(fsList);
-        
+
         assertEquals(11, BowlingScore.calculuateGameScore(scoreSheet));
     }
-    
+
+    @Test
+    public void testFrameTwoBallTwo() {
+        ScoreSheet scoreSheet = new ScoreSheet();
+        List<FrameScore> fsList = new ArrayList<>();
+
+        FrameScore fs1 = new FrameScore();
+        fs1.setFirstBall(3);
+        fs1.setSecondBall(5);
+        fsList.add(fs1);
+
+        FrameScore fs2 = new FrameScore();
+        fs2.setFirstBall(3);
+        fs2.setSecondBall(1);
+        fsList.add(fs2);
+
+        scoreSheet.setFrameScores(fsList);
+
+        assertEquals(12, BowlingScore.calculuateGameScore(scoreSheet));
+    }
+
+    @Test
+    public void testFrameOneStrike() {
+        ScoreSheet scoreSheet = new ScoreSheet();
+        List<FrameScore> fsList = new ArrayList<>();
+
+        FrameScore fs1 = new FrameScore();
+        fs1.setFirstBall(10);
+        fsList.add(fs1);
+
+        FrameScore fs2 = new FrameScore();
+        fs2.setFirstBall(3);
+        fs2.setSecondBall(6);
+        fsList.add(fs2);
+
+        scoreSheet.setFrameScores(fsList);
+
+        assertEquals(28, BowlingScore.calculuateGameScore(scoreSheet));
+    }
+
 }
